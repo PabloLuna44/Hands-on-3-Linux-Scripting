ead3bedc8636effddf20263c7015064fb112e44e
PabloLuna44 <119721670+PabloLuna44@users.noreply.github.com>
2024-09-29 15:35:10 -0600

    Add files via upload

---
+++ Hands-on 3/Linux Script create/create.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Crea un archivo de texto llamado mytext.txt agregando la cadena hola mundo
+echo -e "Create file called mytext.txt adding the string hello to it\n"
+echo "hola mundo" > mytext.txt
+
+#Despliega/Imprime en la terminal el contenido de mytext.txt
+echo -e "Display/Print content of the mytext.txt file\n"
+cat mytext.txt
+
+#Crea un fichero llamado backup
+echo -e "\nCreate a directory called backup\n"
+mkdir backup
+
+#Mover el archivo mytext.txt al subdirectorio backup
+echo -e "Move file mytext.txt to directory backup\n"
+mv mytext.txt backup
+
+#Listar el contenido del directorio backup
+echo -e "List files in the directory backup\n"
+ls backup
+
+#Elimina el archivo mytext del fichero backup
+echo -e "\nDelete mytext.txt file from backup subdirectory\n"
+rm backup/mytext.txt
+
+#Eliminar el fichero backup
+echo -e "Delete backup directory\n"
+rm -r backup
+
+echo -e "\nEnd Script\n"
+read -p "Press enter to continue ..."
+
+
+
---
+++ Hands-on 3/Linux Script helloworld/helloworld.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Imprimir en pantalla helloword
+echo -e "helloword \n"
+
+#Listar el contenido del directorio actual
+echo -e "List of files and folders in the current directory: \n"
+ls
+
+#Crear un fichero llamado test
+echo -e "\nCreate a subdirectory called test \n"
+mkdir test
+
+#Cambiarse al fichero test
+echo -e "\nChange to the test subdirectory"
+cd test
+
+#Listar el contenido del fichero test
+echo -e "\nList of files and folders in the test\n"
+ls
+
+echo "End Script"
+read -p "Press enter to continue ..."
